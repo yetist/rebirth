5c2bc68ccd9ba694ed4a18815155a195b3e57769
Xiaotian Wu <wuxiaotian@loongson.cn>
2023-11-10 16:14:01 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,9 @@ EFI=$(find -name rebirthloongarch64.efi)
 
 if [ "x$EFI" == "x" ];then
     exit
-else
-    EFI_DIR=$(dirname $EFI)
 fi
+
+EFI_DIR=$(dirname $EFI)
 
 cat > ${EFI_DIR}/startup.nsh << EOF
 @echo -off
@@ -15,14 +15,24 @@ fs0:\rebirthloongarch64.efi
 endif
 EOF
 
+cat > ${EFI_DIR}/rebirth.conf << EOF
+title test
+linux   /vmlinuz
+initrd  /initrd.img
+cmdline root=/dev/mem loglevel=7 console=ttyS0,115200
+EOF
+
+cp /boot/vmlinuz-linux ${EFI_DIR}/vmlinuz
+cp /boot/initramfs-linux.img ${EFI_DIR}/initrd.img
+
 qemu-system-loongarch64 \
     -m 4G \
     -cpu la464-loongarch-cpu \
     -machine virt \
-    -smp 1 \
+    -smp 4 \
     -bios /usr/share/edk2/loongarch64/QEMU_EFI.fd \
     -serial stdio \
-    -vga std \
+    -device virtio-gpu-pci \
     -net nic -net user \
     -device nec-usb-xhci,id=xhci,addr=0x1b \
     -device usb-tablet,id=tablet,bus=xhci.0,port=1 \
